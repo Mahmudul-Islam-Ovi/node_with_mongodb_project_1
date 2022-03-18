0353656b8cf9f03d38c4476d69eb8ae73d74df86
Mahmudul Islam <mahmudulislam14270@gmail.com>
2022-03-18 16:51:30 +0600

    Update port

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(cors());
 app.use(express.json());
 
 
-port = process.env.PORT || 5000
+const port = process.env.PORT || 5000
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.imuxo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
